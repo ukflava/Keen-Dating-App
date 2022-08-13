1ef054e2d94af549d796e5f169d91cd799dc18e8
AVacAdok <avakuang8@gmail.com>
2022-08-13 20:40:40 +0000

    change seeds to start with no matches when load the page

--- express-back-end/db/queries/02_seeds.sql
+++ express-back-end/db/queries/02_seeds.sql
@@ -49,11 +49,11 @@ VALUES
 
 INSERT INTO matchings (from_user_id, to_user_id, like_value, seen, matched_date) 
 VALUES 
-(1, 2, true, true, CURRENT_TIMESTAMP),
+
 (2, 1, true, true, CURRENT_TIMESTAMP),
-(1, 4, true, true, CURRENT_TIMESTAMP),
+
 (4, 1, true, true, CURRENT_TIMESTAMP),
-(1, 5, true, true, CURRENT_TIMESTAMP),
+
 (5, 1, true, true, CURRENT_TIMESTAMP),
 
 (3, 1, true, false, CURRENT_TIMESTAMP);
