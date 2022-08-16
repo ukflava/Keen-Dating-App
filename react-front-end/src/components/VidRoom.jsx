3679ddd6eeb3c075854fcdd8e8fc0b1fb6d748d0
josemourinho333 <yoo.phil92@gmail.com>
2022-08-16 08:59:11 -0700

    Update styling

--- react-front-end/src/components/VidRoom.jsx
+++ react-front-end/src/components/VidRoom.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const VidRoom = React.forwardRef((props, ref) => {
   return(
     <div className='absolute inset-0 z-50 bg-gray-500/60 flex flex-col items-center justify-center'>
-      <div className="bg-transparent flex flex-col w-2/3 h-4/5">
+      <div className="bg-transparent flex flex-col max-w-min h-4/5">
       <div className="flex bg-transparent justify-between">
         <button onClick={() => props.setRoomOpen(false)} type="button" className="text-white bg-fuchsia-800 px-2 oy-1 rounded-full max-w-max mb-2">
             X
@@ -13,11 +13,11 @@ const VidRoom = React.forwardRef((props, ref) => {
         </button>
       </div>
 
-        <div className="video-grid bg-white h-full">
-          <div className="my-video border border-fuchsia-800">
+        <div className="video-grid bg-white">
+          <div className="my-video border">
             <video muted={true}/>
           </div>
-          <div className="match-video border border-red-500">
+          <div className="match-video border">
             <video ref={ref.remoteVideoRef} />
           </div>
         </div>
