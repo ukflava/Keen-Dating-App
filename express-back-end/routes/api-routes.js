0a60ac07aefc628e5635c5cdaa0db3e405c7e71f
ukflava <ukflava@gmail.com>
2022-08-15 18:19:21 -0700

    can change preferences and save it

--- express-back-end/routes/api-routes.js
+++ express-back-end/routes/api-routes.js
@@ -275,10 +275,10 @@ router.get("/users/preferences", (req, res) => {
 router.post("/hardreset", (req, res) => {
   
   const query = `
-  UPDATE users
-  SET drink_id=1, exercise_id=4, dating_goal_id=4
+  UPDATE preferences
+  SET gender_id=1, drink_id=1, exercise_id=4, dating_goal_id=4
   WHERE 
-    id = 1
+    user_id = 1
   RETURNING *;
   `
   return db.query(query, )
--- react-front-end/cypress/e2e/02userProfile/03changePreferences.cy.js
+++ react-front-end/cypress/e2e/02userProfile/03changePreferences.cy.js
@@ -5,7 +5,7 @@ context('Change preferences', () => {
     //   // data for post
     // })
 
-    cy.request('POST','/api/hardreset')
+    cy.request('POST','/api/hardreset').wait(1000)
     
     cy.visit("/")
     const login = 'ce@gmail.com'
@@ -14,9 +14,9 @@ context('Change preferences', () => {
     cy.get('input[placeholder="Username/Email"]').type(`${login}{enter}`);
     cy.get('input[placeholder="Password"]').type(`${password}{enter}`).wait(500)
   })
-  beforeEach(() => {
-    cy.visit("/hardreset") 
-  })
+  // beforeEach(() => {
+  //   cy.visit("/hardreset") 
+  // })
 
 
   it("can change preferences and save it", () => {
@@ -24,20 +24,19 @@ context('Change preferences', () => {
     cy.visit("/preferences")
     .contains('Dating Preferences')
     cy.get('div').contains('I want to meet...').click().wait(300)
-    // .contains('Woman').should('be.visible')
-    // .contains('Everyone')
+    cy.get('div').contains('I want to meet...').should('be.visible')
+    cy.get('div').contains('Woman').should('be.visible')
+    cy.get('div').contains('Man').should('be.visible')
+   
     cy.get('input[type="radio"]').check('3').wait(300);
     cy.contains("Everyone").should('be.visible');
     cy.get('div').contains('Someone who drinks...').click().wait(300)
-    // .contains( 'Socially')
-    // .contains( 'Yes')
+    cy.get('div').contains( 'Socially').should('be.visible')
+    cy.get('div').contains( 'Yes').should('be.visible')
     cy.get('input[type="radio"]').check('2').wait(300);
     // cy.contains("Never").should('be.visible');
     cy.get('a').contains('Save').click().wait(300)
-    .contains('update your preferences').should('be.visible')
-
-
-
+    cy.get('div').contains('Check back later to see new people or update your preferences.').should('be.visible')
 
 })
 })
